5d77074b88c80e0bbe62c4ca44240842a7657ae2
saravananmp <mpsaravanan035@gmail.com>
2018-10-12 19:45:18 +0530

    added noop encoder

--- src/main/java/com/oauthserver/oauthserver/config/OAuth2Config.java
+++ src/main/java/com/oauthserver/oauthserver/config/OAuth2Config.java
@@ -1,8 +1,10 @@
 package com.oauthserver.oauthserver.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -12,35 +14,36 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private final AuthenticationManager authenticationManager;
+  @Autowired private final AuthenticationManager authenticationManager;
 
-    @Autowired
-    public OAuth2Config(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
-    }
+  @Autowired
+  public OAuth2Config(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
+  }
 
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                .inMemory()
-                .withClient("alpha")
-                .secret("alpha_secret")
-                .authorizedGrantTypes("authorization_code", "implicit","client_credentials","password")
-                .scopes("Write","Read","All")
-                .redirectUris("https://medium.com")
-                .accessTokenValiditySeconds(3600)
-                .refreshTokenValiditySeconds(6*3600);
-    }
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients
+        .inMemory()
+        .withClient("alpha")
+        .secret("alpha_secret")
+        .authorizedGrantTypes("authorization_code", "implicit", "client_credentials", "password")
+        .scopes("Write", "Read", "All")
+        .redirectUris("https://medium.com")
+        .accessTokenValiditySeconds(3600)
+        .refreshTokenValiditySeconds(6 * 3600);
+  }
 
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer.tokenKeyAccess("permitAll()")
-                .checkTokenAccess("permitAll()").passwordEncoder(NoOpPasswordEncoder.getInstance());
-    }
+  @Override
+  public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+    oauthServer
+        .tokenKeyAccess("permitAll()")
+        .checkTokenAccess("permitAll()")
+        .passwordEncoder(NoOpPasswordEncoder.getInstance());
+  }
 
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-    }
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    endpoints.authenticationManager(authenticationManager);
+  }
 }
